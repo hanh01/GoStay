6e43697a20b7ac15dfdc16cc63fbbb2fe6de5d9f
dohonghanh <dohonghanh2101@gmail.com>
2024-01-16 14:59:32 +0700

    ok

---
+++ GoStay.WebServer/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using GoStay.Peter;
+using HotelApiV56.Api;
+
+
+var builder = WebApplication.CreateBuilder(args);
+// builder.Services.AddSingleton<IHomePageApi>(new HomePageApi(Environment.GetEnvironmentVariable("API_HOST")));
+// builder.Services.AddSingleton<IListingDetailPageApi>(new ListingDetailPageApi(Environment.GetEnvironmentVariable("API_HOST")));
+// builder.Services.AddSingleton<IProfilePageApi>(new ProfilePageApi(Environment.GetEnvironmentVariable("API_HOST")));
+// builder.Services.AddSingleton<ICollectionPageApi>(new CollectionPageApi(Environment.GetEnvironmentVariable("API_HOST")));
+var app=ServiceCollectionExtensions.MakeApp(args, builder);
+
+app.Run();
+
